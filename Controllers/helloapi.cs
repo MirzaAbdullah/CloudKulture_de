b43bdfe77c89b4134ff1c8a9129eab66171b6433
Muhammad Abdullah <mirzaabdullah244@gmail.com>
2020-09-23 23:08:06 +0200

    „3_NewApi“

---
+++ Controllers/helloapi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudKulture_de.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class helloapi : ControllerBase
+    {
+
+    }
+}
